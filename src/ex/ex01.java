ef2157408740346a527e35441f2e8d33a43642dc
Shengjie0266 <105021040@live.asia.edu.tw>
2016-11-14 16:02:40 +0800

    ex
    ex

--- src/ex/ex01.java
+++ src/ex/ex01.java
@@ -1,13 +1,34 @@
 package ex;
+import java.util.Scanner;
 /*
  * Date: 2016/11/14
- * Author: 1050210XX OOO
+ * Author: 105021040 ¬x¸t³Ç
  */
 public class ex01 {
-
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Scanner scn= new Scanner(System.in);
+		int n=scn.nextInt();
+		var(n);
+	}
+	public static void var(int n){
+		Scanner scn=new Scanner(System.in);
+		int sum1=0,sum2=0,a1,a2,a3,a4,t;
+		int data[]=new int [n];
+		for(a1=0;a1<n;a1++){
+			 data[a1]=scn.nextInt();
+		}
+		for(a1=0;a1<n;a1++){
+			sum1+=data[a1];
+		}
+		a3=sum1/n;
+		for(a1=0;a1<n;a1++){
+			a2=a3-data[a1];
+			a4=a2*a2;
+			sum2+=a4;
+		}
+		t=sum2/n;
+		System.out.println(t);
 	}
 
 }
---
+++ src/ex/ex02.java
@@ -0,0 +1,12 @@
+package ex;
+import java.util.Scanner;
+public class ex02 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner scn= new Scanner(System.in);
+		int n=scn.nextInt();
+		
+	}
+
+}
